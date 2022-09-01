c8499e3228525a0b384188661b9196103b631be5
Numbatt <diego.rico10010@gmail.com>
2022-09-01 09:43:51 -0500

    Revert "Update Sidebar.svelte"
    This reverts commit ee95b150157fee6389e3442402b1da296694a3f6.

--- moz-todo-svelte/src/Sidebar.svelte
+++ moz-todo-svelte/src/Sidebar.svelte
@@ -4,7 +4,7 @@
 
 <aside class="absolute w-full h-full bg-blue-200 border-r-2 shadow-lg" class:open>
 	<nav class="p-12 text-xl">
-		<a class="block" href="#about">Aboutt</a>
+		<a class="block" href="#about">About</a>
 		<a class="block" href="contact">Contact</a>
 	</nav>
 </aside>
