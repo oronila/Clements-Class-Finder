d322a5d21d2994afb9d7132a21b35798f4ef311d
Numbatt <diego.rico10010@gmail.com>
2022-09-02 09:48:08 -0500

    made title / logo linkable, made logo square

--- moz-todo-svelte/src/Logo.svelte
+++ moz-todo-svelte/src/Logo.svelte
@@ -1,12 +1,17 @@
 <svg width=500 height=40>
+	<a href="/">
 	<image width=60 height=40 href="clements-logo.png"/>
 	<text x=65 y=27>CLEMENTS CLASS FINDER</text>
+	</a>
 </svg>
 <style>
 	@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 	
 	text {
 		font-family: 'Roboto', sans-serif;
-		fill: currentColor;
+		fill: black;
+	}
+	a:hover{
+		text-decoration: none;
 	}
 </style>
