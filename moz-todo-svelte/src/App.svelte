8a2cf5c0c282655fdaf739db943c8e3bd2a656b8
oronila <noormehdiali@gmail.com>
2022-10-18 09:48:48 -0500

    Update App.svelte

--- moz-todo-svelte/src/App.svelte
+++ moz-todo-svelte/src/App.svelte
@@ -206,9 +206,26 @@
 </svelte:head>
 
 <style>
-	:global(body) {
+	/*:global(body) {
 		padding: 0;
 		overflow: hidden;
+	}*/
+	.map :global(.marker-text) {
+		width:100%;
+		text-align:center;
+		font-weight:600;
+		background-color:#444;
+		color:#EEE;
+		border-radius:0.5rem;
+	}
+	
+	.map :global(.map-marker) {
+		width:30px;
+		transform:translateX(-50%) translateY(-25%);
 	}
 </style>
 
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+   integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+   crossorigin=""/>
+<div class="map" style="height:100%;width:100%" use:mapAction />
