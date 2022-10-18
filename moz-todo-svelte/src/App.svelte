1b417dd813b548e9192a0c7223b4be39b1b003b9
oronila <noormehdiali@gmail.com>
2022-10-18 09:42:04 -0500

    Update App.svelte

--- moz-todo-svelte/src/App.svelte
+++ moz-todo-svelte/src/App.svelte
@@ -26,6 +26,168 @@
 			window.location = "/";
 		}
 	}
+	const markerLocations = [
+		[29.8283, -96.5795],
+		[37.8283, -90.5795],
+		[43.8283, -102.5795],
+		[48.40, -122.5795],
+		[43.60, -79.5795],
+		[36.8283, -100.5795],
+		[38.40, -122.5795],
+	];
+	
+	const initialView = [39.8283, -98.5795];
+	function createMap(container) {
+	  let m = L.map(container, {preferCanvas: true }).setView(initialView, 5);
+    L.tileLayer(
+	    'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+	    {
+	      attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+	        &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+	      subdomains: 'abcd',
+	      maxZoom: 14,
+	    }
+	  ).addTo(m);
+
+    return m;
+  }
+	
+	let eye = true;
+	let lines = true;
+	
+	let toolbar = L.control({ position: 'topright' });
+	let toolbarComponent;
+	toolbar.onAdd = (map) => {
+		let div = L.DomUtil.create('div');
+		toolbarComponent = new MapToolbar({
+			target: div,
+			props: {}
+		});
+
+		toolbarComponent.$on('click-eye', ({ detail }) => eye = detail);
+		toolbarComponent.$on('click-lines', ({ detail }) => lines = detail);
+		toolbarComponent.$on('click-reset', () => {
+			map.setView(initialView, 5, { animate: true })
+		})
+
+		return div;
+	}
+
+	toolbar.onRemove = () => {
+		if(toolbarComponent) {
+			toolbarComponent.$destroy();
+			toolbarComponent = null;
+		}
+	};
+	
+	// Create a popup with a Svelte component inside it and handle removal when the popup is torn down.
+	// `createFn` will be called whenever the popup is being created, and should create and return the component.
+	function bindPopup(marker, createFn) {
+		let popupComponent;
+		marker.bindPopup(() => {
+			let container = L.DomUtil.create('div');
+			popupComponent = createFn(container);
+			return container;
+		});
+
+		marker.on('popupclose', () => {
+			if(popupComponent) {
+				let old = popupComponent;
+				popupComponent = null;
+				// Wait to destroy until after the fadeout completes.
+				setTimeout(() => {
+					old.$destroy();
+				}, 500);
+
+			}
+		});
+	}
+	
+	let markers = new Map();
+	
+	function markerIcon(count) {
+		let html = `<div class="map-marker"><div>${markerIcons.library}</div><div class="marker-text">${count}</div></div>`;
+		return L.divIcon({
+			html,
+			className: 'map-marker'
+		});
+	}
+	
+
+	function createMarker(loc) {
+		let count = Math.ceil(Math.random() * 25);
+		let icon = markerIcon(count);
+		let marker = L.marker(loc, {icon});
+		bindPopup(marker, (m) => {
+			let c = new MarkerPopup({
+				target: m,
+				props: {
+					count
+				}
+			});
+			
+			c.$on('change', ({detail}) => {
+				count = detail;
+				marker.setIcon(markerIcon(count));
+			});
+			
+			return c;
+		});
+		
+		return marker;
+	}
+	
+	function createLines() {
+		return L.polyline(markerLocations, { color: '#E4E', opacity: 0.5 });
+	}
+
+	let markerLayers;
+	let lineLayers;
+  function mapAction(container) {
+    map = createMap(container); 
+		toolbar.addTo(map);
+		
+		markerLayers = L.layerGroup()
+ 		for(let location of markerLocations) {
+ 			let m = createMarker(location);
+			markerLayers.addLayer(m);
+ 		}
+		
+		lineLayers = createLines();
+		
+		markerLayers.addTo(map);
+		lineLayers.addTo(map);
+		
+    return {
+       destroy: () => {
+				 toolbar.remove();
+				 map.remove();
+				 map = null;
+			 }
+    };
+	}
+	
+	// We could do these in the toolbar's click handler but this is an example
+	// of modifying the map with reactive syntax.
+	$: if(markerLayers && map) {
+		if(eye) {
+			markerLayers.addTo(map);
+		} else {
+			markerLayers.remove();
+		}
+	}
+	
+	$: if(lineLayers && map) {
+		if(lines) {
+			lineLayers.addTo(map);
+		} else {
+			lineLayers.remove();
+		}
+	}
+
+	function resizeMap() {
+	  if(map) { map.invalidateSize(); }
+  	}
 </script>
 
 
