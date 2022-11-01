62bb96a7f9f42879d81a4503f1f02b64d1cc3835
oronila <noormehdiali@gmail.com>
2022-11-01 09:38:02 -0500

    padding fix

--- moz-todo-svelte/src/Main.svelte
+++
@@ -1,6 +0,0 @@
-<main class="p-4 text-black-300">
-</main>
-
-<style>
-	
-</style>
--- moz-todo-svelte/src/separatefile.svelte
+++ moz-todo-svelte/src/separatefile.svelte
@@ -11,7 +11,7 @@
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
+		
 		max-width: 240px;
 		margin: 0 auto;
 	}
